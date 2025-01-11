2e42f512ab44c7637ab8163b46359bb1e48fa53e
Miuponn <kellygao@live.ca>
2025-01-11 01:08:48 -0500

    Initial Commit

---
+++ PinkyPromise/PinkyPromiseApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinkyPromiseApp.swift
+//  PinkyPromise
+//
+//  Created by Kelly Gao on 2025-01-11.
+//
+
+import SwiftUI
+
+@main
+struct PinkyPromiseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PinkyPromiseTests/PinkyPromiseTests.swift
@@ -0,0 +1,17 @@
+//
+//  PinkyPromiseTests.swift
+//  PinkyPromiseTests
+//
+//  Created by Kelly Gao on 2025-01-11.
+//
+
+import Testing
+@testable import PinkyPromise
+
+struct PinkyPromiseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
